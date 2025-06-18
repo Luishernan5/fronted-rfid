6a5a9d069c43cc27fee2f7252423072252ec44ba
jesus <angelhernan12486@gmail.com>
2025-06-18 04:23:47 -0600

    first commit

---
+++ js/datosSimulados.js
@@ -0,0 +1,47 @@
+const usuariosSimulados = [
+  {
+    id_usuario: 1,
+    matricula: "1",
+    password: "1",
+    rol: "alumno",
+    nombre: "ANGEL EDUARDO",
+    ape1: "ANGELES",
+    ape2: "MIRANDA",
+    correo: "angel@ejemplo.com",
+    grupo: "3401",
+    materias: [
+      "ACF-0905",
+      "SCC-1017",
+      "SCD-1027",
+      "AEF-1031",
+      "SCA-1026",
+      "SCD-1003",
+      "ACA-0907",
+      "INGLES"
+    ]
+  },
+  {
+    matricula: "2",
+    password: "2",
+    rol: "profesor",
+    nombre: "Rodolfo",
+    ape1: "Alcantara",
+    ape2: "Rosales",
+    correo: "rodolfo@ejemplo.com",
+    materias: [
+      {
+        clave: "ACF-0905",
+        nombre: "Ecuaciones Diferenciales"
+      }
+    ]
+  },
+  {
+    matricula: "3",
+    password: "3",
+    rol: "admin",
+    nombre: "Administrador",
+    ape1: "",
+    ape2: "",
+    correo: "admin@tesji.edu.mx"
+  }
+];
---
+++ js/login.js
@@ -0,0 +1,45 @@
+document.getElementById("loginForm").addEventListener("submit", async function(e) {
+  e.preventDefault();
+
+  const rol = document.getElementById("rol").value;
+  const matricula = document.getElementById("matricula").value;
+  const password = document.getElementById("password").value;
+
+  const loginData = {
+    usuario: matricula,
+    password: password,
+    rol: parseInt(rol)
+  };
+
+  try {
+    const response = await fetch("http://localhost:8000/login/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(loginData)
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.detail || "Error en las credenciales");
+    }
+
+    const data = await response.json();
+    console.log("Respuesta del servidor:", data);
+
+    if (data.message === "Login exitoso") {
+      localStorage.setItem("usuario", JSON.stringify(data));
+      
+      // Redirigir según rol
+      switch(data.rol) {
+        case 1: window.location.href = "alumno.html"; break;
+        case 2: window.location.href = "dashboard-profesor.html"; break;
+        case 3: window.location.href = "admin_panel.html"; break;
+      }
+    } else {
+      document.getElementById("error").textContent = "Credenciales incorrectas.";
+    }
+  } catch (error) {
+    console.error("Error al hacer login:", error);
+    document.getElementById("error").textContent = error.message;
+  }
+});
